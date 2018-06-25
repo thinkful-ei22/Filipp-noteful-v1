56c8586be8dc950b79b856df6dca080feaef0eda
Filipp Gorbunov <Filippgorb@gmail.com>
2018-06-25 12:24:44 -0700

    updated server to support a search

--- server.js
+++ server.js
@@ -13,7 +13,15 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
-  res.json(data);
+  const searchTerm = req.query.searchTerm;
+
+  if (searchTerm) {
+    res.json(data.filter(item => item.title.includes(searchTerm)));
+  }
+  else {
+    res.json(data);
+  }
+  
 });
 
 app.get('/api/notes/:id', (req, res) => {
