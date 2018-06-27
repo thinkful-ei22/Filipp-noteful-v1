d7f620cd2731ec944b3e7839b0fd4ed76a87437c
Filipp Gorbunov <Filippgorb@gmail.com>
2018-06-27 11:49:45 -0700

    Moved endpoints to express router

---
+++ router/notes.router.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+// Load array of notes
+const data = require('../db/notes');
+
+// Simple In-Memory Database
+const simDB = require('../db/simDB');
+const notes = simDB.initialize(data);
+
+router.get('/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
+
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err); // goes to error handler
+    }
+    res.json(list); // responds with filtered array
+  });
+});
+
+router.get('/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+  
+  notes.find(id, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    res.json(item);
+  });
+});
+
+router.put('/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  notes.update(id, updateObj, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+module.exports = router;
